1d966980a0203828fdb7faeac46752776aefb070
longmgi <longnguyen.mgi@gmail.com>
2021-12-29 10:05:39 +0700

    scroll smooth

--- main.js
+++ main.js
@@ -22,6 +22,29 @@ function toggleNav(){
     }
 }
 // Scroll top
+// function goTop() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+function goTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollY will be -Infinity
+            scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+            if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = scrollY;
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
 
 // function prevSlide(){
 //     var currentSlide = document.querySelector(".slideactive");
